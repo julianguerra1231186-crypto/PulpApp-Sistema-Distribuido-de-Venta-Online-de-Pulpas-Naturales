0cfe5b9389f352dc3026147bcceea27094b3a3d8
julianguerra1231186-crypto <julianguerra1231186@gmail.com>
2026-03-02 00:00:27 -0500

    Feat(pulpapp.ms_users):add PostgreSQL y docker funcionando con validaciones

--- ms-users/src/main/java/com/pulpapp/ms_users/dto/UserRequestDTO.java
+++ ms-users/src/main/java/com/pulpapp/ms_users/dto/UserRequestDTO.java
@@ -1,4 +1,21 @@
 package com.pulpapp.ms_users.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class UserRequestDTO {
+
+    @NotBlank(message = "El nombre no puede estar vacío")
+    private String name;
+
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "Debe ser un email válido")
+    private String email;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 6, message = "La contraseña debe tener mínimo 6 caracteres")
+    private String password;
 }
--- ms-users/src/main/java/com/pulpapp/ms_users/dto/UserResponseDTO.java
+++ ms-users/src/main/java/com/pulpapp/ms_users/dto/UserResponseDTO.java
@@ -1,4 +1,11 @@
 package com.pulpapp.ms_users.dto;
 
+import lombok.Data;
+
+@Data
 public class UserResponseDTO {
+
+    private Long id;
+    private String name;
+    private String email;
 }
