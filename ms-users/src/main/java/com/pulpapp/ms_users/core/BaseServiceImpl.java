f2a164ff0b232a546dee31bf9a394a972a020ca8
julianguerra1231186-crypto <julianguerra1231186@gmail.com>
2026-03-24 11:52:17 -0500

    refactor(ms-users): mejora en servicios, excepciones y mapper

--- ms-users/src/main/java/com/pulpapp/ms_users/core/BaseServiceImpl.java
+++ ms-users/src/main/java/com/pulpapp/ms_users/core/BaseServiceImpl.java
@@ -1,18 +1,23 @@
 package com.pulpapp.ms_users.core;
 
-import com.pulpapp.ms_users.mapper.EntityMapper;
 import com.pulpapp.ms_users.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public abstract class BaseServiceImpl<T, D, S, R extends JpaRepository<T, Long>>
         implements IBaseService<T, D, S> {
 
     protected final R repository;
-    protected final EntityMapper<T, D, S> mapper;
+
+    // Funciones de mapeo (MapStruct)
+    protected abstract Function<T, D> toResponseMapper();
+    protected abstract Function<S, T> toEntityMapper();
+    protected abstract void updateEntityFromDto(S dto, T entity);
 
     protected abstract String getEntityName();
 
@@ -20,8 +25,8 @@ public abstract class BaseServiceImpl<T, D, S, R extends JpaRepository<T, Long>>
     public List<D> findAll() {
         return repository.findAll()
                 .stream()
-                .map(mapper::toResponseDto)
-                .toList();
+                .map(toResponseMapper())
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -30,13 +35,13 @@ public abstract class BaseServiceImpl<T, D, S, R extends JpaRepository<T, Long>>
                 .orElseThrow(() ->
                         new ResourceNotFoundException(getEntityName() + " not found with id: " + id));
 
-        return mapper.toResponseDto(entity);
+        return toResponseMapper().apply(entity);
     }
 
     @Override
     public D save(S dto) {
-        T entity = mapper.toEntity(dto);
-        return mapper.toResponseDto(repository.save(entity));
+        T entity = toEntityMapper().apply(dto);
+        return toResponseMapper().apply(repository.save(entity));
     }
 
     @Override
@@ -45,8 +50,8 @@ public abstract class BaseServiceImpl<T, D, S, R extends JpaRepository<T, Long>>
                 .orElseThrow(() ->
                         new ResourceNotFoundException(getEntityName() + " not found with id: " + id));
 
-        mapper.updateEntityFromDto(dto, entity);
-        return mapper.toResponseDto(repository.save(entity));
+        updateEntityFromDto(dto, entity);
+        return toResponseMapper().apply(repository.save(entity));
     }
 
     @Override
